2c7a7388fdf16f907d13d37bdc4009bcb6527acf
Quoc Hung <hungtranpc1342k6@gmail.com>
2026-07-31 10:47:03 +0700

    feat(routes): refactor AppRouter component to encapsulate routing logic

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,9 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage } from "../features/auth/LoginPage";
 import { RegisterPage } from "../features/auth/RegisterPage";
 
-<BrowserRouter>
-  <Routes>
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/register" element={<RegisterPage />} />
-  </Routes>
-</BrowserRouter>;
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        {/* thêm /products, /cart, /orders sau khi viết xong các trang đó */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
